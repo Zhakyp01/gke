528170597e8a8752f330d49cfa5a67a59e4bf661
Zhakyp <73534500+Zhakyp01@users.noreply.github.com>
2023-03-30 16:18:02 +0600

    Update cluster.tf

--- cluster.tf
+++ cluster.tf
@@ -24,7 +24,7 @@ resource "google_container_cluster" "cluster" {
   initial_node_count       = 1
 
   ip_allocation_policy{
-    cluster_secondary_range_name = var.cluster_secondary_name
+#     cluster_secondary_range_name = var.cluster_secondary_name
     services_secondary_range_name = var.cluster_service_name
   }
   
